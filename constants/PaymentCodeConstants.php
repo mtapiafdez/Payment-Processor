26c6242e61f8a2d2c002af716c83c52f3c98fd76
Miguel Augusto Tapia <mtapiafdez@gmail.com>
2020-06-25 16:17:40 -0400

    Update PaymentCodeConstants.php

--- constants/PaymentCodeConstants.php
+++ constants/PaymentCodeConstants.php
@@ -2,6 +2,6 @@
 class PaymentCodeConstants
 {
     // My Merchant Credentials
-    const MERCHANT_LOGIN_ID = "6vV7xX7h";
-    const MERCHANT_TRANSACTION_KEY = "49C3y6dGc887xGYj";
+    const MERCHANT_LOGIN_ID = "LOGINID";
+    const MERCHANT_TRANSACTION_KEY = "KEY";
 }
